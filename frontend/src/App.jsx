1c72bd8090f0ea6afb91bb600cb74d71ba1e4b6e
riya-tech02 <riyashukla9453347726@icloud.com>
2026-01-14 19:19:58 +0530

    FIX: Change backtick to parenthesis in fetch call

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ export default function App() {
     setLoading(true);
     
     try {
-      const res = await fetch(`${API}/message`, {
+      const res = await fetch(`${API}/message`,  {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ 
